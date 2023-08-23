5da02df6b6e11334e03e8743eaab3ca155133f9b
Jingwen123456 <133638604+Jingwen123456@users.noreply.github.com>
2023-08-23 21:29:43 +0800

    Initial commit

---
+++ TixplorerCoreFrontend/Models/DTO/myFavoriteVM.cs
@@ -0,0 +1,15 @@
+﻿using NuGet.Protocol.Plugins;
+
+namespace TixplorerCoreFrontend.Models.DTO
+{
+    public class myFavoriteVM
+    {
+            public string Account {get; set;}
+            public string Pid { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public decimal? Discount { get; set; }
+            public int Stock { get; set; }
+            public string? Image { get; set; }
+        }
+}
